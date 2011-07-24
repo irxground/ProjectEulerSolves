671a38d0aa211bae4160d117df437b01769643e4
irxground <irxnjhtchlnrw@googlemail.com>
2011-07-24 19:25:09 +0900

    Cleanup code.

--- haskell/problem56.hs
+++ haskell/problem56.hs
@@ -1,9 +1,9 @@
 import Char
 
-sumDigit num = sum [digitToInt c | c <- show num]
+sumDigit = sum . map digitToInt . show
 
 ans = maximum [sumDigit (a^b) | a <- [1..100], b <- [1..100]]
 
-main = putStrLn $ show ans
+main = print ans
 
 
